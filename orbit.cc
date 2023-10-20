49092c81a71f6f542026e2ecf522ba2254ba842b
RonNiles <ron.niles@memverge.com>
2023-10-20 12:51:49 -0700

    Test table-free triangle counting

--- orbit.cc
+++ orbit.cc
@@ -90,7 +90,10 @@ static uint64_t SortNibbles(uint64_t word) {
 
 std::unordered_map<uint64_t, uint64_t> itemcounts;
 
+std::unordered_map<uint64_t, uint64_t> trcounts;
+
 static uint64_t CountEdges(uint64_t x) {
+  uint64_t sav_x = x;
   uint64_t val = edge_counts[0][x % 512].val;
   x >>= 9;
   val += edge_counts[1][x % 512].val;
@@ -98,8 +101,96 @@ static uint64_t CountEdges(uint64_t x) {
   val += edge_counts[2][x % 512].val;
   x >>= 9;
   val += edge_counts[3][x % 512].val;
+  EdgeCount ec{val};
   val = SortNibbles(val);
   ++itemcounts[val];
+  x = ~sav_x;
+  uint64_t tv = 0;
+  if ((x & 0x7) == 0) tv +=  0x111;
+  if ((x & 0x19) == 0) tv +=  0x1011;
+  if ((x & 0x2a) == 0) tv +=  0x1101;
+  if ((x & 0x34) == 0) tv +=  0x1110;
+  if ((x & 0xc1) == 0) tv +=  0x10011;
+  if ((x & 0x142) == 0) tv +=  0x10101;
+  if ((x & 0x184) == 0) tv +=  0x10110;
+  if ((x & 0x248) == 0) tv +=  0x11001;
+  if ((x & 0x290) == 0) tv +=  0x11010;
+  if ((x & 0x320) == 0) tv +=  0x11100;
+  if ((x & 0xc01) == 0) tv +=  0x100011;
+  if ((x & 0x1402) == 0) tv +=  0x100101;
+  if ((x & 0x1804) == 0) tv +=  0x100110;
+  if ((x & 0x2408) == 0) tv +=  0x101001;
+  if ((x & 0x2810) == 0) tv +=  0x101010;
+  if ((x & 0x3020) == 0) tv +=  0x101100;
+  if ((x & 0x4440) == 0) tv +=  0x110001;
+  if ((x & 0x4880) == 0) tv +=  0x110010;
+  if ((x & 0x5100) == 0) tv +=  0x110100;
+  if ((x & 0x6200) == 0) tv +=  0x111000;
+  if ((x & 0x18001) == 0) tv +=  0x1000011;
+  if ((x & 0x28002) == 0) tv +=  0x1000101;
+  if ((x & 0x30004) == 0) tv +=  0x1000110;
+  if ((x & 0x48008) == 0) tv +=  0x1001001;
+  if ((x & 0x50010) == 0) tv +=  0x1001010;
+  if ((x & 0x60020) == 0) tv +=  0x1001100;
+  if ((x & 0x88040) == 0) tv +=  0x1010001;
+  if ((x & 0x90080) == 0) tv +=  0x1010010;
+  if ((x & 0xa0100) == 0) tv +=  0x1010100;
+  if ((x & 0xc0200) == 0) tv +=  0x1011000;
+  if ((x & 0x108400) == 0) tv +=  0x1100001;
+  if ((x & 0x110800) == 0) tv +=  0x1100010;
+  if ((x & 0x121000) == 0) tv +=  0x1100100;
+  if ((x & 0x142000) == 0) tv +=  0x1101000;
+  if ((x & 0x184000) == 0) tv +=  0x1110000;
+  if ((x & 0x600001) == 0) tv +=  0x10000011;
+  if ((x & 0xa00002) == 0) tv +=  0x10000101;
+  if ((x & 0xc00004) == 0) tv +=  0x10000110;
+  if ((x & 0x1200008) == 0) tv +=  0x10001001;
+  if ((x & 0x1400010) == 0) tv +=  0x10001010;
+  if ((x & 0x1800020) == 0) tv +=  0x10001100;
+  if ((x & 0x2200040) == 0) tv +=  0x10010001;
+  if ((x & 0x2400080) == 0) tv +=  0x10010010;
+  if ((x & 0x2800100) == 0) tv +=  0x10010100;
+  if ((x & 0x3000200) == 0) tv +=  0x10011000;
+  if ((x & 0x4200400) == 0) tv +=  0x10100001;
+  if ((x & 0x4400800) == 0) tv +=  0x10100010;
+  if ((x & 0x4801000) == 0) tv +=  0x10100100;
+  if ((x & 0x5002000) == 0) tv +=  0x10101000;
+  if ((x & 0x6004000) == 0) tv +=  0x10110000;
+  if ((x & 0x8208000) == 0) tv +=  0x11000001;
+  if ((x & 0x8410000) == 0) tv +=  0x11000010;
+  if ((x & 0x8820000) == 0) tv +=  0x11000100;
+  if ((x & 0x9040000) == 0) tv +=  0x11001000;
+  if ((x & 0xa080000) == 0) tv +=  0x11010000;
+  if ((x & 0xc100000) == 0) tv +=  0x11100000;
+  if ((x & 0x30000001) == 0) tv +=  0x100000011;
+  if ((x & 0x50000002) == 0) tv +=  0x100000101;
+  if ((x & 0x60000004) == 0) tv +=  0x100000110;
+  if ((x & 0x90000008) == 0) tv +=  0x100001001;
+  if ((x & 0xa0000010) == 0) tv +=  0x100001010;
+  if ((x & 0xc0000020) == 0) tv +=  0x100001100;
+  if ((x & 0x110000040) == 0) tv +=  0x100010001;
+  if ((x & 0x120000080) == 0) tv +=  0x100010010;
+  if ((x & 0x140000100) == 0) tv +=  0x100010100;
+  if ((x & 0x180000200) == 0) tv +=  0x100011000;
+  if ((x & 0x210000400) == 0) tv +=  0x100100001;
+  if ((x & 0x220000800) == 0) tv +=  0x100100010;
+  if ((x & 0x240001000) == 0) tv +=  0x100100100;
+  if ((x & 0x280002000) == 0) tv +=  0x100101000;
+  if ((x & 0x300004000) == 0) tv +=  0x100110000;
+  if ((x & 0x410008000) == 0) tv +=  0x101000001;
+  if ((x & 0x420010000) == 0) tv +=  0x101000010;
+  if ((x & 0x440020000) == 0) tv +=  0x101000100;
+  if ((x & 0x480040000) == 0) tv +=  0x101001000;
+  if ((x & 0x500080000) == 0) tv +=  0x101010000;
+  if ((x & 0x600100000) == 0) tv +=  0x101100000;
+  if ((x & 0x810200000) == 0) tv +=  0x110000001;
+  if ((x & 0x820400000) == 0) tv +=  0x110000010;
+  if ((x & 0x840800000) == 0) tv +=  0x110000100;
+  if ((x & 0x881000000) == 0) tv +=  0x110001000;
+  if ((x & 0x902000000) == 0) tv +=  0x110010000;
+  if ((x & 0xa04000000) == 0) tv +=  0x110100000;
+  if ((x & 0xc08000000) == 0) tv +=  0x111000000;
+  ++trcounts[tv];
   return val;
 }
 
@@ -162,8 +253,20 @@ int main(int argc, char *argv[]) {
       printf("%d ", ec.get(i));
       sum += ec.get(i);
     }
-    printf("(%lu)\n", val.second);
+    printf(" %lX (%lu)\n", val.first, val.second);
     ++validate[sum];
   }
+  printf("Triangles %ld\n", trcounts.size());
+#if 0
+  for (const auto &val : trcounts) {
+    EdgeCount ec{val.first};
+    unsigned sum = 0;
+    for (int i = 0; i < 9; ++i) {
+      printf("%d ", ec.get(i));
+      sum += ec.get(i);
+    }
+    printf(" %lX (%lu)\n", val.first, val.second);
+  }
+#endif
   return 0;
 }
